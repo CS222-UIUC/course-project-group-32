ed40574252dd3baa13ad17c5159965b307f88b5a
Raul Daniel Higareda <raul.d.higareda@gmail.com>
2022-09-25 18:02:05 -0500

    Implement tests, linter, and searchbar

--- artsi-website/src/components/NavBar.js
+++ artsi-website/src/components/NavBar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import logo from "./artsi-logo.png";
 import './NavBar.css';
 import { Button } from './Button';
+import SearchBar from './SearchBar';
+import Data from './Data.json'
 
 function NavBar() {
     const imgStyle = {
@@ -35,6 +37,9 @@ function NavBar() {
                 </div>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <div className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <SearchBar placeholder="Search Artsi..." data={Data}/>
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
