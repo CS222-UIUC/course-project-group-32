ada8fc9da8fc23426137e0f1151535a9ac9774e9
Raul Daniel Higareda <raul.d.higareda@gmail.com>
2022-12-06 17:41:58 -0600

    Fix css issues

--- artsi-website/src/components/pages/Tutorial.test.js
+++
@@ -1,9 +0,0 @@
-import { getByTestId, render } from "@testing-library/react";
-import Tutorial from "./Tutorial";
-
-
-test("tutorial exists", () => {
-    const { getByRole } = render(<Tutorial />);
-    const name = getByRole('tutorial-exists');
-    expect(name).toBeInTheDocument()
-});
